f9e9fa70d8b3d01dfd4cae7fd53599db012e6f52
Mor-Bar <142618614+Mor-Bar@users.noreply.github.com>
2024-07-29 00:57:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+''' 
+Installed 2 package(s) with pip: Matplotlib, Pandas
+1 - Matplotlib: to plot and see the graph of the data
+2 - Pandas: to read the data from the csv file and manipulate it
+'''
